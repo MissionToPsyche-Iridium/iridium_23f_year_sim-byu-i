688b3cbe2f65f0ea435607e633a7e4784e3202ce
Brodie M Busby <93949221+brodiembusby@users.noreply.github.com>
2024-10-31 11:37:07 -0600

    Drawing Page

--- pysche/src/App.js
+++ pysche/src/App.js
@@ -1,23 +1,21 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from './components/footer';
-import Header from './components/header';
-
+import Layout from './components/layout';
+import DrawingPage from './components/drawingPage';
 function App() {
   return (
 <Router>
-  <Header />
-  <main>
-    <h1>Hello</h1>
+  <Layout>
     <Routes>
       <Route path='/' elements = {<h1>Home Page</h1>} />
       <Route path="/Faq" element={<h1>FAQ</h1>} />
-    </Routes>
-  </main>
-  <Footer />
+      <Route path="/DrawingPage" element={<DrawingPage />} />
+      </Routes>
+    </Layout>
 </Router>
   );
 }
 
 export default App;
+// Change the routes for home page and faq into actual react pages instead of <h1>....
---
+++ pysche/src/components/drawingPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Canvas from "./canvas";
+const DrawingPage = () => (
+<main>
+    <Canvas />
+</main>
+)
+
+export default DrawingPage
