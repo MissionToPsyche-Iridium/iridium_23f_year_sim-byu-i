322a28b3c592e891d07a1899406bc3d505ba1c3b
brodiembusby <brodiembusby@hotmail.com>
2024-11-09 14:56:24 -0700

    Added Pages Folder for pages

---
+++ pysche/src/components/hints.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const hintBox = () => (
+<div className="hintBox">
+    <div className="hintBoxWords">
+     <button>
+        THis is button
+        </button>   
+    </div>
+
+</div>
+)
+
+export default hintBox
