2d99e7f00920278dacd6abef7cc2c43d08d0ce12
Thomas Jamieson <jamiesonthomas314@gmail.com>
2024-11-09 18:04:44 -0700

    I have made it so that when a month is selected the number of days that are in that month are populated into the days dropdown. This includes February and leap years.

--- pysche/src/components/triviaComponents/ageConverter.js
+++ pysche/src/components/triviaComponents/ageConverter.js
@@ -12,13 +12,10 @@ function AgeConverter() {
 
     const years = [];
 
-    // const thirtys = ["Apr", "Jun", "Sep", "Nov"];
+    const thirties = ["Apr", "Jun", "Sep", "Nov"];
 
-    // const thirtyOnes = ["Jan", "Mar", "May", "Jul", "Aug", "Nov", "Dec"];
+    const thirtyOnes = ["Jan", "Mar", "May", "Jul", "Aug", "Nov", "Dec"];
     
-
-    // const daysInMonth = thirtyOnes.includes(selectedMonth) ? 31 : thirtys.includes(selectedMonth) ? 30;
-
 
     for (let year = currentYear; year >= lastYear; --year){
         years.push(year);
@@ -30,8 +27,16 @@ function AgeConverter() {
     
     const [selectedYear, setYear] = useState("");
     
-
     
+
+
+    function leapYear(year){
+        // console.log(year)
+        return(year % 400 === 0 ? 29 : year % 100 === 0 ? 28 : year % 4 === 0 ? 29 : 28)
+    }
+    
+    const daysInMonth = thirtyOnes.includes(selectedMonth) ? 31 : thirties.includes(selectedMonth) ? 30 : leapYear(Number(selectedYear));
+
 
     function changeMonth(event){
         setMonth(event.target.value);
@@ -43,6 +48,7 @@ function AgeConverter() {
 
     function changeYear(event){
         setYear(event.target.value);
+
     }
 
     
@@ -69,40 +75,10 @@ function AgeConverter() {
                 <option value="Dec">December</option>
             </select>
             {/* <p>Selected Month {selectedMonth}</p> */}
-            {selectedMonth === "Feb" ? <p>true</p> : <p>false</p>}
+            {/* {daysInMonth} */}
             <select id="days" value={selectedDay} onChange={changeDay}>
                 <option value="Day">Day</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-                <option value="21">21</option>
-                <option value="22">22</option>
-                <option value="23">23</option>
-                <option value="24">24</option>
-                <option value="25">25</option>
-                <option value="26">26</option>
-                <option value="27">27</option>
-                <option value="28">28</option>
-                <option value="29">29</option>
-                <option value="30">30</option>
-                <option value="31">31</option>
+                {Array.from({ length: daysInMonth}, (_, i) => <option value={i+1}>{i+1}</option>)}
 
             </select>
             {/* <p>Selected Day {selectedDay}</p> */}
