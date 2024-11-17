275b0d2c98533177dd14b9cd024675c0a51691ff
jcwestover <107649547+jcwestover@users.noreply.github.com>
2024-11-16 18:14:39 -0800

    finished WeightConverter component. also had to make changes to calcPsycheWeight function - fixes #87

--- pysche/src/components/weightConverter.js
+++ pysche/src/components/weightConverter.js
@@ -1,17 +1,37 @@
-import React from "react";
-import "./../componentsCSS/trivia.css"
-
+import React, { useState } from "react";
+import { Container, Typography, Box } from "@mui/material";
+import WeightSubmit from "./weightSubmit";
+import WeightInput from "./weightInput";
+import UnitSwitch from "./unitSwitch";
+import { calcPsycheWeight } from "../utils/conversions";
 
 function WeightConverter() {
-    return (
-     
-        <div class="box">
+  const [isKg, setIsKg] = useState(false);
+  const [weight, setWeight] = useState("");
+  const [psycheWeight, setPsycheWeight] = useState(null);
 
-            weight converter
-                
-        </div>
-        
-    );
+  const handleWeightChange = (value) => {
+    setWeight(value);
+  };
+
+  const handleUnitToggle = (checked) => {
+    setIsKg(checked);
+  };
+
+  const handleCalculate = () => {
+    const result = calcPsycheWeight(weight, isKg);
+    setPsycheWeight(result);
+  };
+
+  return (
+    <div>
+      <h1>Weight Converter</h1>
+      <UnitSwitch isKg={isKg} onToggle={handleUnitToggle} />
+      <WeightInput weight={weight} isKg={isKg} onChange={handleWeightChange} />
+      <WeightSubmit onClick={handleCalculate} />
+      <p>Your weight on Psyche: {psycheWeight} {isKg ? 'kg' : 'lbs'}</p>
+    </div>
+  );
 }
 
-export default WeightConverter
+export default WeightConverter;
--- pysche/src/utils/conversions.js
+++ pysche/src/utils/conversions.js
@@ -1,12 +1,13 @@
 export { calcPsycheWeight };
 
-function calcPsycheWeight(earthWeight) {
-    // converts given Earth weight into Psyche weight. This function can take lbs or kg for parameter and will spit out the respective unit. (we can add a button later that will dictate what unit the user is inputting later)
+function calcPsycheWeight(earthWeight, isKg) {
+    // converts given Earth weight into Psyche weight. This function can take lbs or kg for parameter and will spit out the respective unit. (we can add a button later that will dictate what unit the user is inputting later).
     // parametes: 
     //     earthWeight: weight on Earth (kg or lbs)
     // return:
     //     weight on Psyche (in respective unit)
-    const psycheWeight = earthWeight * (0.144 / 9.8);
-    return Number(psycheWeight.toFixed(2));
+    const weightInKg = isKg ? parseFloat(earthWeight) : parseFloat(earthWeight) * 0.453592;
+    const psycheWeight = weightInKg * (0.144 / 9.8);
+    return parseFloat(psycheWeight.toFixed(2));
 }
 
