49574cc9d338308b28b4f7ab425c241241efcacc
jcwestover <107649547+jcwestover@users.noreply.github.com>
2024-11-16 16:13:07 -0800

    created weightSubmit component and added functionality - fixes #85, fixes #86

--- pysche/src/pages/triviaPage.js
+++ pysche/src/pages/triviaPage.js
@@ -2,11 +2,13 @@ import React from "react";
 // import BasicSwitch from "../../weightSwitch.js";
 import WeightInput from "../components/weightInput.js";
 import UnitSwitch from "../components/unitSwitch.js"
+import WeightSubmit from "../components/weightSubmit.js";
 
 const TriviaPage = () => (
   <main>
     <WeightInput />
     <UnitSwitch />
+    <WeightSubmit />
   </main>
 );
 
