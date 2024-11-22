134d004e0ab7f5c11a5b216935f832bb0ee148fc
jcwestover <107649547+jcwestover@users.noreply.github.com>
2024-11-22 15:47:20 -0800

    Added comments on the code I have written - fixes #114

--- pysche/src/components/weightInput.js
+++ pysche/src/components/weightInput.js
@@ -3,6 +3,9 @@
 import React from "react";
 import { TextField } from "@mui/material";
 
+// WeightInput Component
+
+// This component is an input field that will take a weight given by a user that will later be used to calculate what the given weight will be on Psyche 
 function WeightInput({ weight, isKg, onChange  }) {
     return (
         <TextField 
--- pysche/src/components/weightSubmit.js
+++ pysche/src/components/weightSubmit.js
@@ -3,6 +3,10 @@
 import React from "react";
 import { Button } from '@mui/material';
 
+
+// WeightSubmit Component
+
+// This component creates a submit button that when clicked will submit the weight given in the WeightInput component and will use the calcPsycheWeight function from the conversions.js file. 
 function WeightSubmit({ onClick }) {
     return (
         <Button
