02ef9da975804d2d4c82a9fb5a10aede9c3da32f
Thomas Jamieson <jamiesonthomas314@gmail.com>
2024-11-22 21:52:55 -0700

    A lot of time was taken to figure out the drop down icon. But I now have a basic accordion.

--- psyche/src/components/psycheCost.js
+++ psyche/src/components/psycheCost.js
@@ -1,10 +1,38 @@
 import React from "react";
+import "../componentsCSS/trivia.css"
+
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 function PsycheCost() {
     return (
      
-        <div>
-            Psyche Cost
+        <div class="box">
+            <Accordion>
+                <AccordionSummary expandIcon={<ArrowDropDownIcon/>}>
+                    How Much is Psyche Worth? And What Could You Buy?
+
+                </AccordionSummary>
+
+                <AccordionDetails>
+                    It is estimated to be a whole lot. Also just as a disclaimer all of these values are just estimates.
+                </AccordionDetails>
+                
+                <Accordion>
+                    <AccordionSummary expandIcon={<ArrowDropDownIcon/>}>
+                        Paper
+
+                    </AccordionSummary>
+
+                    <AccordionDetails>
+                        You could buy.
+                    </AccordionDetails>
+
+                </Accordion>
+            </Accordion>
+            
                 
         </div>
         
