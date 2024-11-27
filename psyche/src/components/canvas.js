76373671d0637dca4d03023f5d4428cf82e619d6
Brodie M Busby <93949221+brodiembusby@users.noreply.github.com>
2024-11-27 13:24:42 -0700

    Tried Refactoring dont think I changed much

--- psyche/src/components/canvas.js
+++ psyche/src/components/canvas.js
@@ -1,5 +1,5 @@
 // All Tools and Canvas Functions for Draw Page
-import React, { useRef, useState, forwardRef, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Tooltip from '@mui/material/Tooltip';
 import HintBox from "../components/hints";
@@ -14,7 +14,7 @@ import { FaPaintbrush } from "react-icons/fa6";
 import "../index.css";
 
 function Canvas() {
-  // useStates 
+  // useStates/Setters
   const [color, setColor] = useState("#000000");
   const [brushSize, setBrushSize] = useState(2); //New state for the brushes
   const [isDrawing, setIsDrawing] = useState(false); // New state for drawing with the brushes
@@ -33,23 +33,19 @@ function Canvas() {
   const navigate = useNavigate(); // corectly sends the user to the preview page after submiting
   const canvasRef = useRef(null);
 
-  //TODO: Find a way so the canvas automatically changes with the screen. 
-  // This works for the most part except everthing else in the screen would need to change
-  // as well for it to look good 
-  // TODO: Take out this comment when after explainging to group
+  // Getters
+  const getCanvasContext = () => {
+    const canvas = canvasRef.current;
+    return canvas ? canvas.getContext("2d", { willReadFrequently: true }) : null;
+  };
 
-  // useEffect(() => {
-  //   const resizeCanvas = () => {
-  //     const width = Math.max(300, window.innerWidth * 0.5); 
-  //     const height = Math.max(300, window.innerHeight * 0.5); 
-  //     setCanvasSize({ width, height });
-  //   };
+  const getMousePosition = (event) => {
+    const rect = canvasRef.current.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
 
-  //   resizeCanvas();
-  //   window.addEventListener("resize", resizeCanvas);
-  //   return () => window.removeEventListener("resize", resizeCanvas);
-  // }, []);
-
+    return { x, y };
+  };
 
   const isActive = {
     fill: isFilling,
@@ -61,7 +57,6 @@ function Canvas() {
   // Useful for activating Brush Color
   const isAnyActive = isFilling || isSpraying || isErasing || isOval;
 
-
   // Checks if tool is true or not and then based on the tool toggles true or false
   const toggleTool = (tool) => {
 
@@ -70,25 +65,46 @@ function Canvas() {
     setIsErasing(tool === "erase" ? !isActive.erase : false);
     setIsOval(tool === "oval" ? !isActive.oval : false);
   };
+  // Handles
+  const handleMouseMove = (event) => {
+    if (!isDrawing) return;
 
+    if (isSpraying) {
+      sprayPaint(event);
+    } else if (isOval) {
+      // Preview logic for drawing oval if needed.
+    } else {
+      draw(event);
+    }
+  };
   const handleSubmit = () => {
     const canvas = canvasRef.current;
     const imageData = canvas.toDataURL("image/png"); // Save the canvas content as a data URL
     navigate("/preview", { state: { image: imageData } }); // Pass it to the preview page
   };
 
+  const handleCanvasClick = (event) => {
+    if (!isFilling) return;
 
+    // Tried changing to getMousePosition but it caused a crash for me
+    const rect = canvasRef.current.getBoundingClientRect();
+    const x = Math.floor(event.clientX - rect.left);
+    const y = Math.floor(event.clientY - rect.top);
+
+    fillArea(x, y, hexToRgb(color));
+  }; // controls the fill tool
+
+  // Drawing Specific Functions
   const startDrawing = (event) => {
     setIsDrawing(true);
-    const canvas = canvasRef.current;
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    const ctx = getCanvasContext();
+    if (!ctx) return;
+
+    const { x, y } = getMousePosition(event);
 
     if (isOval) {
       setStartPoint({ x, y }); // Store the start point for the oval
     } else {
-      const ctx = canvas.getContext("2d");
       ctx.beginPath();
       ctx.moveTo(x, y);
     }
@@ -112,16 +128,13 @@ function Canvas() {
   const draw = (event) => {
     if (!isDrawing || isFilling || isSpraying) return;
 
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
+    const ctx = getCanvasContext();
 
     ctx.lineWidth = brushSize;
     ctx.lineCap = "round";
     ctx.strokeStyle = isErasing ? "rgb(241, 236, 228)" : color;
 
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    const { x, y } = getMousePosition(event);
 
     ctx.lineTo(x, y);
     ctx.stroke();
@@ -130,13 +143,10 @@ function Canvas() {
   const sprayPaint = (event) => {
     if (!isDrawing || !isSpraying) return;
 
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
+    const ctx = getCanvasContext();
     ctx.fillStyle = color;
 
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    const { x, y } = getMousePosition(event);
 
     const sprayDensity = 10;
 
@@ -154,49 +164,12 @@ function Canvas() {
 
 
   const clearCanvas = () => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const ctx = getCanvasContext();
+    ctx.clearRect(0, 0, canvasSize.width, canvasSize.height);
     saveCanvasState(); // Save the cleared state
   }; // controls the clear tool
 
-  const handleCanvasClick = (event) => {
-    if (!isFilling) return;
-
-    const rect = canvasRef.current.getBoundingClientRect();
-    const x = Math.floor(event.clientX - rect.left);
-    const y = Math.floor(event.clientY - rect.top);
-
-    fillArea(x, y, hexToRgb(color));
-  }; // controls the fill tool
-
-  const fillArea = (x, y, fillColor) => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
-
-    const targetColor = getPixelColor(data, x, y, canvas.width);
-    if (colorsMatch(targetColor, fillColor)) return; // Avoid redundant fill
-
-    const stack = [[x, y]];
-
-    while (stack.length) {
-      const [curX, curY] = stack.pop();
-      const idx = (curY * canvas.width + curX) * 4;
-
-      if (colorsMatch(getPixelColor(data, curX, curY, canvas.width), targetColor)) {
-        setPixelColor(data, idx, fillColor);
-        stack.push([curX + 1, curY], [curX - 1, curY], [curX, curY + 1], [curX, curY - 1]);
-      }
-    } // makes sure the fill tool does corect action
-
-    ctx.putImageData(imageData, 0, 0);
-    saveCanvasState(); // Save the state after the fill operation
-  };
-
-
+  // Used for Fill Tool
   const getPixelColor = (data, x, y, width) => {
     const idx = (y * width + x) * 4;
     return [data[idx], data[idx + 1], data[idx + 2], data[idx + 3]];
@@ -209,6 +182,31 @@ function Canvas() {
     data[idx + 3] = 255;
   };
 
+  const fillArea = (x, y, fillColor) => {
+    const ctx = getCanvasContext();
+
+    const imageData = ctx.getImageData(0, 0, canvasSize.width, canvasSize.height);
+    const data = imageData.data;
+
+    const targetColor = getPixelColor(data, x, y, canvasSize.width);
+    if (colorsMatch(targetColor, fillColor)) return; // Avoid redundant fill
+
+    const stack = [[x, y]];
+
+    while (stack.length) {
+      const [curX, curY] = stack.pop();
+      const idx = (curY * canvasSize.width + curX) * 4;
+
+      if (colorsMatch(getPixelColor(data, curX, curY, canvasSize.width), targetColor)) {
+        setPixelColor(data, idx, fillColor);
+        stack.push([curX + 1, curY], [curX - 1, curY], [curX, curY + 1], [curX, curY - 1]);
+      }
+    } // makes sure the fill tool does corect action
+
+    ctx.putImageData(imageData, 0, 0);
+    saveCanvasState(); // Save the state after the fill operation
+  };
+
   const colorsMatch = (color1, color2) => {
     return color1[0] === color2[0] && color1[1] === color2[1] && color1[2] === color2[2];
   };
@@ -219,13 +217,10 @@ function Canvas() {
   };
 
   const drawOval = (event) => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    const rect = canvas.getBoundingClientRect();
+    const ctx = getCanvasContext();
 
-    const endX = event.clientX - rect.left;
-    const endY = event.clientY - rect.top;
-    const { x: startX, y: startY } = startPoint;
+    const { x: endX, y: endY } = getMousePosition(event); // Get mouse position
+    const { x: startX, y: startY } = startPoint; // Start point from state  
 
     const centerX = (startX + endX) / 2;
     const centerY = (startY + endY) / 2;
@@ -254,16 +249,15 @@ function Canvas() {
     setRedoStack((prev) => [lastState, ...prev]); // Add it to the redo stack
     setHistory([...history]); // Update the history
 
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
+    const ctx = getCanvasContext();
     const img = new Image();
 
     // Load the previous state or clear if none
     const previousState = history[history.length - 1] || "";
     img.src = previousState;
     img.onload = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
-      if (previousState) ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvasSize.width, canvasSize.height); // Clear canvas
+      if (previousState) ctx.drawImage(img, 0, 0, canvasSize.width, canvasSize.height);
     };
   };
 
@@ -274,14 +268,13 @@ function Canvas() {
     setHistory((prev) => [...prev, nextState]); // Add it back to the history
     setRedoStack([...redoStack]); // Update redo stack
 
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
+    const ctx = getCanvasContext();
     const img = new Image();
 
     img.src = nextState; // Load the redo state
     img.onload = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvasSize.width, canvasSize.height); // Clear canvas
+      ctx.drawImage(img, 0, 0, canvasSize.width, canvasSize.height);
     };
   };
 
@@ -323,21 +316,18 @@ function Canvas() {
                 <IoColorFill style={{ fontSize: "1.5rem" }} />}  Fill
             </button>
           </Tooltip>
-
           <Tooltip title="Eraser">
             <button className="canvasButton" onClick={() => toggleTool("erase")}>
               {isErasing ? <FaEraser style={{ fontSize: "2rem", color: "#9C3852" }} /> :
                 <FaEraser style={{ fontSize: "1.5rem" }} />} Erase
             </button>
           </Tooltip>
-
           <Tooltip title="Spray">
             <button className="canvasButton" onClick={() => toggleTool("spray")}>
               {isSpraying ? <GiSpray style={{ fontSize: "2rem", color: "#9C3852" }} />
                 : <GiSpray style={{ fontSize: "1.5rem" }} />} Spray
             </button>
           </Tooltip>
-
           <Tooltip title="Oval">
             <button className="canvasButton" onClick={() => toggleTool("oval")}>
               {isOval ? (
@@ -347,17 +337,14 @@ function Canvas() {
               )} Oval
             </button>
           </Tooltip>
-
           {/* Undo and Redo buttons */}
 
           <Tooltip title="Undo">
             <button className="canvasButton" onClick={undo}><LuUndo2 /></button>
           </Tooltip>
-
           <Tooltip title="Redo">
             <button className="canvasButton" onClick={redo}><LuRedo /></button>
           </Tooltip>
-
           <button className="canvasButton" onClick={clearCanvas}>Clear</button>
           <button onClick={handleSubmit} className="canvasButton">
             Submit Drawing
@@ -374,14 +361,10 @@ function Canvas() {
           onMouseDown={(e) => { isFilling ? handleCanvasClick(e) : startDrawing(e); }}
           onMouseUp={endDrawing}
           onMouseOut={endDrawing}
-          onMouseMove={(e) => {
-            if (isSpraying) sprayPaint(e);
-            else if (!isOval) draw(e);
-          }}
+          onMouseMove={handleMouseMove}
         />
         <div>
           {/* Import Hint Box at bottom of page*/}
-
           <HintBox />
         </div>
       </div>
